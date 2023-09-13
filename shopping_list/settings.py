e83a017ca26c175fd6c867b338f174b7b7dbc53b
fikriwahab <fikrimassaidwahab2@gmail.com>
2023-09-14 00:30:01 +0700

    Update Tutorial 2

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -83,7 +83,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# Set database settings automatically using DATABASE_URL.
+if PRODUCTION:
+    DATABASES = {
+        'default': env.db('DATABASE_URL')
+    }
+    DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
